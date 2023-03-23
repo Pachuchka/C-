a3dab307d920947fb36ed73c244751ad5f593947
Pachuchka <pachuchka@gmail.com>
2023-03-23 21:27:54 +0800

    Lesson № 6

---
+++ Lesson № 6/Task 41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int UserNumInput(string message)
+{
+   Console.WriteLine(message);
+   string str = Console.ReadLine();
+   int result = Convert.ToInt32(str);
+   return result;
+}
+int[] CreateArray(int length)
+{
+    int[] array = new int[length];
+    for (int i=0;i<array.Length;i++)
+    {
+        array[i] = UserNumInput($"Введите {i+1}-й элемент");
+    }
+    return array;
+}
+
+int PositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i=0;i<array.Length;i++)
+    {
+        if (array[i]>0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int length = UserNumInput("Массив какой длинны необходимо создать?");
+int[] array =  CreateArray(length);
+Console.WriteLine($"{string.Join(" ", array)} содержит {PositiveNumbers(array)} положительных значений");
---
+++ Lesson № 6/Task 43/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+double UserNumInput(string message)
+{
+   Console.WriteLine(message);
+   string str = Console.ReadLine();
+   double result = Convert.ToDouble(str);
+   return result;
+}
+double [] LineParam(int length)
+{
+    double[] array = new double[length];
+    for (int i=0;i<array.Length;i++)
+    {
+        array[i] = UserNumInput($"Введите {i+1}-й элемент");
+    }
+    return array;
+}
+
+double [] LineIntersection(double [] a, double [] b)
+{
+    double [] result = new double[2];
+    double [] c = new double[2];
+    c[0] = a[0]-b[0];
+    c[1] = a[1]-b[1];
+    result[0] = -1*c[1]/c[0];
+    result[1] = result[0]*a[0]+a[1];
+    return result;
+}
+int length = 2;
+Console.WriteLine("в уравнении вида y=kx+b; 1-й элемент - это k второй элемент - это b");
+double [] a = LineParam(length);
+double [] b = LineParam(length);
+Console.WriteLine($" точкой пересечения линий y={a[0]}*x + {a[1]} и y={b[0]}*x + {b[1]} является точка ({LineIntersection(a,b)[0]},{LineIntersection(a,b)[1]})");
